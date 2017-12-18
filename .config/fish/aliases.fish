311bb7c6f4e44ef3cb96e59412770c4b500c0bfe
Guillaume Astier <astier.gui@gmail.com>
2017-12-18 18:03:50 +0100

    Create aliases.fish

---
+++ .config/fish/aliases.fish
@@ -0,0 +1,56 @@
+# Default
+alias cls='clear'
+alias ps='ps aux'
+alias x='exit'
+alias s='sudo -i'
+alias df='df -H'
+alias du='du -ch'
+alias cp='cp -iv'
+alias rm='rm -i'
+alias mv='mv -iv'
+alias mkdir='mkdir -p'
+alias ping='ping -c 4'
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias aptlist='cat /etc/apt/sources.list{,.d/*} 2>/dev/null | grep -v "^#"'
+alias myip='curl http://ipecho.net/plain; echo'
+
+# Compatibility
+alias del='rm'
+alias dir='ls -la'
+
+# Listing
+alias ls='ls -F --color=always'
+alias ll='ls -lSrh'
+alias lt='ls -alt'
+alias la='ls -A'
+alias lla='ls -Alh'
+alias lat='ls -alt'
+alias ltr='ls -altr'
+alias latr='ls -altr'
+
+# System
+alias shutdown='sudo shutdown –h now'
+alias restart='sudo shutdown –r now'
+alias suspend='sudo pm-suspend'
+alias lock='gnome-screensaver-command --lock'
+
+# Directory
+alias documents='cd ~/Documents'
+alias downloads='cd ~/Téléchargements'
+alias desktop='cd ~/Bureau'
+alias music='cd ~/Musique'
+alias videos='cd ~/Videos'
+
+alias www='cd /var/www'
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias .......='cd ../../../../../..'
+alias ........='cd ../../../../../../..'
+
+alias clearlog='sudo find /var/log/ \( -iregex ".*\.[0-9]+" -o -iname "*.gz" \) -exec rm {} \;'

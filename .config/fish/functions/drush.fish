d39893b0d4bcae4908a84a58cc733be0e4dc7755
Guillaume Astier <astier.gui@gmail.com>
2017-12-21 16:06:19 +0100

    Create drush.fish

---
+++ .config/fish/functions/drush.fish
@@ -0,0 +1,7 @@
+#!/usr/bin/env fish
+
+function __complete_drush
+  drush --early=includes/complete.inc (commandline -cpo) (commandline -t)|cut -d " " -f 1
+end
+
+complete -x -c drush -d "Drush" -a "(__complete_drush)"

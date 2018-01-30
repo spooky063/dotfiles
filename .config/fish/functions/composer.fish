5b43c18800916255e7f8e11a35a2e8ce339b68f7
Guillaume Astier <astier.gui@gmail.com>
2018-01-30 11:27:00 +0100

    Update composer.fish

--- .config/fish/functions/composer.fish
+++ .config/fish/functions/composer.fish
@@ -1,8 +1,15 @@
 function composer
-	docker run \
-      --tty \
-      --interactive \
-      --rm \
-      --volume $PWD:/app \
-      composer $argv
+  set tty --tty
+  docker run \
+    $tty \
+    --interactive \
+    --rm \
+    --net=host \
+    --user (id -u):(id -g) \
+    --volume /etc/passwd:/etc/passwd:ro \
+    --volume /etc/group:/etc/group:ro \
+    --volume (pwd):/app \
+    --volume /home/(id -un):/home/(id -un) \
+    -e COMPOSER_HOME="/home/(id -un)/.composer" \
+    composer $argv
 end

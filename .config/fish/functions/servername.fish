ef6ad8d1dddf8f8cce189cf0ac3806c86760e1a5
Guillaume Astier <astier.gui@gmail.com>
2018-04-30 17:21:55 +0200

    Update servername.fish

--- .config/fish/functions/servername.fish
+++ .config/fish/functions/servername.fish
@@ -1,3 +1,9 @@
 function servername
-    host (dig +short $argv)  | awk '{print $5}' | awk '{ print substr($0,1,length($0)-1) }'
+    if begin; test (command curl -o -I -L -s -w '%{http_code}\n' $argv) -eq 200; end
+        host (dig +short $argv)  | awk '{print $5}' | awk '{ print substr($0,1,length($0)-1) }'
+    else if begin; test (command curl -o -I -L -s -w '%{http_code}\n' $argv) -eq 401; end
+        echo 'Website is protected'
+    else
+        echo 'Website is down'
+    end
 end
